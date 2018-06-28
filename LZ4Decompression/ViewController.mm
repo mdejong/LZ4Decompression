e94a0223060c992314827d66fe551da4c71ad8f4
Mo DeJong <mo@modejong.com>
2018-06-27 18:49:03 -0700

    enable stepping through denom array to adjust for more CPU usage at each step

--- LZ4Decompression/ViewController.mm
+++ LZ4Decompression/ViewController.mm
@@ -74,6 +74,7 @@ vector<uint8_t> compressedWithLZ4HC(const vector<uint8_t> & inBytes) {
 
 @property (nonatomic, assign) int numerator;
 @property (nonatomic, assign) int denominator;
+@property (nonatomic, assign) int step;
 
 @property (nonatomic, retain) IBOutlet UILabel *fpsLabel;
 
@@ -127,38 +128,14 @@ vector<uint8_t> compressedWithLZ4HC(const vector<uint8_t> & inBytes) {
   
   self.lz4DecompressedData = [NSMutableData dataWithLength:unencodedData.length];
   
-  // Schedule callback at 30 FPS
-  
-//  float interval = 1.0/30.0;
-//  float interval = 1.0/45.0;
-//  float interval = 1.0/59.0;
-//  float interval = 1.0/60.0;
-  
-//  float interval = 1.0/120.0;
-//  float interval = 1.0/240.0;
-  
-//  float interval = 1.0/400.0;
-//  float interval = 1.0/800.0;
-  
-//  float interval = 1.0/1300.0; // 1293 at 85% CPU
-//  float interval = 1.0/2000.0; // 1350 at 90% CPU
-  //float interval = 1.0/2500.0; // 1404 at 90% CPU
-  
-  //float interval = 1.0/3000.0; // 1404 at 93% CPU
-  
-  //float interval = 1.0/5000.0; // 1404 at 93% CPU
-  
-  //float interval = 1.0/35000.0; // 1425 at 98% CPU
-  
-//  float interval = 1.0/50000.0; // 1491 at 98% CPU
-  
-//  float interval = 1.0/100000.0; // 1500 at 98% CPU on A9
-
 //  self.numerator = 1;
 //  self.denominator = 30;
-  
+
   self.numerator = 1;
-  self.denominator = 100000;
+  self.denominator = 60;
+  
+//  self.numerator = 1;
+//  self.denominator = 100000;
   
   float interval = (float)self.numerator/(float)self.denominator;
   
@@ -178,19 +155,41 @@ vector<uint8_t> compressedWithLZ4HC(const vector<uint8_t> & inBytes) {
 }
 
 - (void) secondElapsedTimer {
-  NSLog(@"secondElapsedTimer : %8d bytes : aka %d MB", self.secondByteCounter, self.secondByteCounter/(1024*1024));
+  NSLog(@"secondElapsedTimer : Denom %8d : %12d bytes : aka %4d MB", self.denominator, self.secondByteCounter, self.secondByteCounter/(1024*1024));
   
   int megabytes = self.secondByteCounter/(1024*1024);
   
   self.fpsLabel.text = [NSString stringWithFormat:@"%d", self.denominator];
   self.mbLabel.text = [NSString stringWithFormat:@"%d", megabytes];
   
-  if ((0)) {
-    // Reschedule
+  if ((1)) {
+    // Reschedule each second
     [self.intervalTimer invalidate];
     
     self.numerator = 1;
-    self.denominator = 60;
+    
+    NSArray *stepDenom = @[@(30), @(60), @(120), @(240), @(800), @(1400), @(2500), @(5000), @(20000), @(50000), @(70000), @(100000)];
+    
+    self.step = (self.step + 1) % [stepDenom count];
+    
+    NSNumber *denomNum = stepDenom[self.step];
+    
+    self.denominator = [denomNum intValue];
+    
+    // A9
+    
+//    Denom       30 :   91226112 bytes : aka   90 MB (12% CPU)
+//    Denom       60 :  185597952 bytes : aka  177 MB (17% CPU)
+//    Denom      120 :  374341632 bytes : aka  357 MB
+//    Denom      240 :  748683264 bytes : aka  714 MB
+//    Denom      800 : 1198522368 bytes : aka 1143 MB
+//    Denom     1400 : 1431306240 bytes : aka 1365 MB
+//    Denom     2500 : 1453326336 bytes : aka 1386 MB
+//    Denom     5000 : 1519386624 bytes : aka 1449 MB
+//    Denom    20000 : 1550843904 bytes : aka 1479 MB (90% CPU)
+//    Denom    50000 : 1560281088 bytes : aka 1488 MB
+//    Denom    70000 : 1560281088 bytes : aka 1488 MB
+//    Denom   100000 : 1560281088 bytes : aka 1488 MB
     
     float interval = (float)self.numerator/(float)self.denominator;
     
@@ -199,6 +198,7 @@ vector<uint8_t> compressedWithLZ4HC(const vector<uint8_t> & inBytes) {
                                                         selector:@selector(benchmarkTimer)
                                                         userInfo:nil
                                                          repeats:YES];
+    
   }
 
   self.secondByteCounter = 0;
