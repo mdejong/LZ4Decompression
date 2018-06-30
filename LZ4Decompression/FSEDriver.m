1e41fef14a1363742f85b4f6b80edfba881e9f02
Mo DeJong <mo@modejong.com>
2018-06-30 02:56:50 -0700

    fix compression ratio in FSEDriver by breaking stream into blocks, new encoding bytes where the base byte from a block is appeneded as deltas, update README with new compression and threading results

--- LZ4Decompression/FSEDriver.m
+++ LZ4Decompression/FSEDriver.m
@@ -14,6 +14,8 @@
 
 #include "fse.h"
 
+#define FSE_BLOCK_SIZE_SYMBOL (128 * 1024)
+
 // FSEDriver
 
 @interface FSEDriver ()
@@ -21,6 +23,147 @@
 @end
 
 @implementation FSEDriver
+
+// Compress unencodedData and return compressed data in a buffer
+
+- (NSData*) compressData:(NSData*)unencodedData
+{
+  int numBlocks = (int)unencodedData.length / FSE_BLOCK_SIZE_SYMBOL;
+  if ((unencodedData.length % FSE_BLOCK_SIZE_SYMBOL) != 0) {
+    numBlocks += 1;
+  }
+  
+  NSMutableData *allCompressedBytes = [NSMutableData data];
+  
+  NSMutableArray *mArrOfSizes = [NSMutableArray array];
+  
+  for ( int blocki = 0; blocki < numBlocks; blocki++ ) {
+    // Allocate a dst location as large as the src to handle a worst case, it should not be close.
+    
+    int blockSizeInBits = FSE_BLOCK_SIZE_SYMBOL;
+    if (blocki == (numBlocks - 1)) {
+      blockSizeInBits = (int)unencodedData.length - (FSE_BLOCK_SIZE_SYMBOL * blocki);
+    }
+    
+    uint8_t *inBytesPtr = (uint8_t *) unencodedData.bytes;
+    inBytesPtr += (FSE_BLOCK_SIZE_SYMBOL * blocki);
+    
+    size_t worstCompressedCaseSizeInBytes = FSE_compressBound(blockSizeInBits);
+    
+    NSMutableData *compressedBytes = [NSMutableData dataWithLength:worstCompressedCaseSizeInBytes];
+    
+    //NSLog(@"compress from %d to %d (%d bytes)", (FSE_BLOCK_SIZE_SYMBOL * blocki), (FSE_BLOCK_SIZE_SYMBOL * blocki)+blockSizeInBits, blockSizeInBits);
+    
+    size_t compSize = FSE_compress((void*) compressedBytes.mutableBytes, (size_t)compressedBytes.length,
+                                   (const void*) inBytesPtr, blockSizeInBits);
+    
+    if (FSE_isError(compSize)) {
+      printf("FSE error : %s\n", FSE_getErrorName(compSize));
+      assert(0);
+    }
+    
+    [compressedBytes setLength:compSize];
+    
+    [allCompressedBytes appendData:compressedBytes];
+    
+    //NSLog(@"compress block to %d bytes", (int)compSize);
+    
+    [mArrOfSizes addObject:@(compSize)];
+  }
+  
+  // Finally append N block sizes to the binary data, the decoder can figure out how many
+  // there are from the input size
+  
+  for ( NSNumber *sizeNum in mArrOfSizes ) {
+    uint32_t blockSize = [sizeNum unsignedIntValue];
+    [allCompressedBytes appendBytes:&blockSize length:sizeof(uint32_t)];
+  }
+  
+  return allCompressedBytes;
+}
+
+// Decompress encodedData into buffer, returns TRUE on success and FALSE on failure
+
+- (BOOL) decompressData:(NSData*)encodedData buffer:(uint8_t*)buffer length:(int)length
+{
+  // Break into blocks of compressed data using a table at the end of the input data.
+  
+  int numBlocks = length / FSE_BLOCK_SIZE_SYMBOL;
+  if ((length % FSE_BLOCK_SIZE_SYMBOL) != 0) {
+    numBlocks += 1;
+  }
+  
+  uint8_t *encodedDataPtr = (uint8_t *) encodedData.bytes;
+  
+  int headerNumBytes = numBlocks * sizeof(uint32_t);
+  
+  uint32_t header[numBlocks];
+  memcpy(&header[0], encodedDataPtr + (int)encodedData.length - headerNumBytes, headerNumBytes);
+  
+  int blockStartOffset = 0;
+  
+  uint8_t *outBufferPtr = buffer;
+  
+#define DISPATCH_BLOCKS_GCD
+  
+#if defined(DISPATCH_BLOCKS_GCD)
+  dispatch_group_t group = dispatch_group_create();
+  dispatch_queue_t queue = dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_HIGH, 0);
+#endif // DISPATCH_BLOCKS_GCD
+  
+  for (int blocki = 0; blocki < numBlocks; blocki++ ) {
+    int blockNumBytes = header[blocki];
+    
+    int decompressedBlockSize = FSE_BLOCK_SIZE_SYMBOL;
+    if (blocki == (numBlocks - 1)) {
+      decompressedBlockSize = length - (blocki * FSE_BLOCK_SIZE_SYMBOL);
+    }
+    
+    const void * encodedBlock = encodedDataPtr + blockStartOffset;
+    
+    // Block capture pointers that will be decoded
+    
+    void (^DecompressBlock)(void) = ^void (void)
+    {
+      size_t result = FSE_decompress(outBufferPtr, decompressedBlockSize,
+                                     encodedBlock, (size_t)blockNumBytes);
+      
+      if (FSE_isError(result)) {
+        printf("FSE error : %s\n", FSE_getErrorName(result));
+        assert(0);
+      }
+      
+#if defined(DEBUG)
+      assert(result == decompressedBlockSize);
+#endif // DEBUG
+    };
+    
+#if defined(DISPATCH_BLOCKS_GCD)
+    dispatch_group_async(group, queue, ^{
+      DecompressBlock();
+    });
+#else // DISPATCH_BLOCKS_GCD
+    DecompressBlock();
+#endif // DISPATCH_BLOCKS_GCD
+    
+    blockStartOffset += blockNumBytes;
+    outBufferPtr += decompressedBlockSize;
+  }
+  
+#if defined(DISPATCH_BLOCKS_GCD)
+  dispatch_group_wait(group, DISPATCH_TIME_FOREVER);
+  //dispatch_release(group);
+#endif // DISPATCH_BLOCKS_GCD
+  
+#if defined(DEBUG)
+  assert((int)(outBufferPtr - buffer) == length);
+#endif // DEBUG
+  
+  return TRUE;
+}
+
+
+/*
 
 // Compress unencodedData and return compressed data in a buffer
 
@@ -63,5 +206,7 @@
   
   return TRUE;
 }
+ 
+*/
 
 @end
